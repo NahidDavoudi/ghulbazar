2ca3064c4663d926fb2846fd20135bd673e4ed7d
Nahid Davoudi <sir.davoudi@gmail.com>
2026-05-21 16:44:21 +0330

    Add login method to AuthService: Implement user login logic with error handling and success response, enhancing user authentication capabilities alongside existing registration functionality.

--- api/app/Modules/Auth/AuthService.php
+++ api/app/Modules/Auth/AuthService.php
@@ -2,6 +2,7 @@
 namespace App\Modules\Auth;
 use App\Modules\User\UserModel;
 use App\Core\Http\Response;
+
 class AuthService
 {
     protected UserModel $userModel;
@@ -9,32 +10,47 @@ class AuthService
     {
         $this->userModel = new UserModel();
     }
-    public function register($data){
+    public function register($data)
+    {
         $user = $this->userModel->findByPhone($data['phone']);
-        if(!$user) {
-            Response::error('User not found', 404);
+        if ($user) {
+            Response::error('User with this phone already exists', 422);
         }
-        $data =[
+        $userData = [
             'phone' => $data['phone'],
-            'password' => password_hash($data['password'] , PASSWORD_DEFAULT),
+            'password' => password_hash($data['password'], PASSWORD_DEFAULT),
             'full_name' => $data['full_name'],
             'role' => 'costumer'
         ];
-        try{
-            $userId = $this->userModel->create($data);
-            $result =  [
+        try {
+            $userId = $this->userModel->create($userData);
+            $result = [
                 'user_id' => $userId,
                 'message' => 'Registration completed successfully.'
             ];
-            Response::success($result ,200);
-        } catch (\Exception $e){
+            Response::success($result, 200);
+        } catch (\Exception $e) {
             $result = [
                 'user_id' => null,
                 'message' => $e->getMessage()
             ];
             Response::error($result);
-        } 
-       
+        }
+    }
 
+    public function login($data)
+    {
+        $user = $this->userModel->findByPhone($data['phone']);
+        if (!$user) {
+            Response::error('User not found', 404);
+        }
+        if (!password_verify($data['password'], $user['password'])) {
+            Response::error('Invalid password', 401);
+        }
+        $result = [
+            'user_id' => $user['id'],
+            'message' => 'Login successful.'
+        ];
+        Response::success($result, 200);
     }
 }
