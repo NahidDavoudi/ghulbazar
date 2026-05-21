bba6e0ea31ad1b61a31d231635c406d5202219df
Nahid Davoudi <sir.davoudi@gmail.com>
2026-05-21 16:19:22 +0330

    Initialize UserModel in AuthService: Set up UserModel instance within AuthService constructor for user-related operations.

--- api/app/Modules/Auth/AuthService.php
+++ api/app/Modules/Auth/AuthService.php
@@ -1,6 +1,12 @@
 <?php
-
+namespace App\Modules\Auth;
+use App\Modules\User\UserModel;
 class AuthService
 {
-
+    protected UserModel $userModel;
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+    
 }
